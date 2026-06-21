ff1c7c15990eac07ebe1f986b1bfb81159895fa4
Jairo <jairomiguelrodo@gmail.com>
2026-06-21 02:21:31 -0500

    feature: correccion bd

--- backend/db/database.py
+++ backend/db/database.py
@@ -5,7 +5,7 @@ from backend.models.base import Base
 
 # Render te da la variable DATABASE_URL en las Environment Variables
 SQLALCHEMY_DATABASE_URL = os.getenv(
-    "postgresql://admin:sYHl0aPOLaOhSneQCAzigNG73k4qyZyI@dpg-d8rnrubeo5us73djui90-a.oregon-postgres.render.com/kelpie_bd"
+    "DATABASE_URL"
     )
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
